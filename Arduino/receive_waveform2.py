5620f6deb5f96dd1fe7c976775ae6ee6805deb9f
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2024-03-08 13:47:48 -0500

    Update receive_waveform2.py
    Finished inputting rest of current code on the Raspberry Pi for receiving the ble signal.  Not working, needs adjustment.  Current errors include NoneType for the p.delegate.data and print statements meant to check where the code is going not being output to terminal

--- Arduino/receive_waveform2.py
+++ Arduino/receive_waveform2.py
@@ -14,4 +14,18 @@ p = Peripheral("0C:CF:6F:54:6C:60")
 p.setDelegate(MyDelegate())
 
 try:
-  service = p.getServiceByUUID("50
+  service = p.getServiceByUUID("50e967b4-2065-4356-8a56-c839b3c32117")
+  characteristic = service.getCharacteristics("2A37")[0]
+  start_time = time.time()
+  while True:
+    if p.waitForNotifications(1.0):
+      print("here")
+      continue
+    print("Waiting...")
+    if time.time() - start_time > 10:
+      break
+finally:
+  p.disconnect()
+
+plt.plot(p.delegate.data)
+plt.show()
