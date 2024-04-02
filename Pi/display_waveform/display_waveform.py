aad1fd0797fc00ad4e093c88875244dad03383ac
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2024-04-02 15:35:49 -0400

    Update display_waveform.py

--- Pi/display_waveform/display_waveform.py
+++ Pi/display_waveform/display_waveform.py
@@ -4,24 +4,16 @@ from matplotlib.animation import FuncAnimation
 
 plt.style.use("fivethirtyeight") # Check styles later
 
-x_vals = []
-y_vals = []
-
 # Live Plotting Function
 def animate(i):
-  data = pd.read_csv('heartData.csv')
-  data = data.iloc[-50:]
+  data = pd.read_csv('heartData.csv').tail(50)
   x = data['iteration']
   y = data['reading']
-
   plt.cla()
-
   plt.plot(x,y, label='Heartbeat')
-
   plt.legend(loc='upper left')
   plt.tight_layout()
 
 ani = FuncAnimation(plt.gcf(), animate, interval=2)
 
-plt.tight_layout()
 plt.show()
